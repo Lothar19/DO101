90dacdeaab10c692f8dc92ec67102335ad33b549
Lothar19 <Lothar19_@hotmail.com>
2020-04-30 06:47:18 +0200

    changes

---
+++ app2.js
@@ -0,0 +1,16 @@
+console.log("Hello DO101 Apps!\n"); 
+
+var express = require('express');
+app = express();
+
+app.get('/', function(req, res) {
+    res.send('Hello New World!\n');
+});
+
+app.get('/moon', function(req, res) {
+    res.send('Hello Moon!\n');
+});
+
+app.listen(8081, function() {
+    console.log("Example app listening on port 8081!\n"); 
+});
